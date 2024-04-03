6d7d9820393f602cc10d2993adf0cab0879bcb20
Fxc7 <farhanxcode7@gmail.com>
2024-04-03 17:56:24 +0700

    Update 03 April 2024

--- configs/env.ts
+++ configs/env.ts
@@ -1,6 +1,6 @@
-export const token = '7145654564:AAFg2xEo6IuomPY-zaDVfB586YqYPMzvPh0';
-export const secret_key = '5a21fc9cece69a5214f3750d780d5e7966138541397fd1b46584f905d4b0603b';
+export const token = 'YOUR TOKEN BOT';
+export const secret_key = '';
 export const prefix = '/';
-export const apikey = 'frhn';
+export const apikey = 'API KEY';
 export const baseURL = 'https://xcoders-api-fxc7.koyeb.app';
-export const owner = ['xcoders_id', 'coders_storeID'];
+export const owner = [];
